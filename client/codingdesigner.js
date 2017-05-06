9acab19857995b43fe419d5ddecf239887ca67ad
Mason Wendell <mason@thecodingdesigner.com>
2017-05-06 00:41:52 -0400

    sass compiling

--- client/codingdesigner.js
+++ client/codingdesigner.js
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from 'react-router-dom'
-import css from './styles/style.css';
+import scss from './styles/style.scss';
 
 // import components
 import Main from './components/Main';
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var webpack = require('webpack');
 
-console.log(path.join(__dirname, 'client'));
+// var PATHS = {
+//   // breakpoint: path.resolve(__dirname, 'node_modules/breakpoint-sass/stylesheets/'),
+//   // modularScale: path.resolve(__dirname, 'node_modules/modularscale-sass/stylesheets'),
+//   // config: path.resolve(__dirname, 'config'),
+//   // font: path.resolve(__dirname, 'lib/client/fonts'),
+//   // image: path.resolve(__dirname, 'lib/client/images'),
+//   // lib: path.resolve(__dirname, 'lib'),
+//   // normalize: path.resolve(__dirname, 'node_modules/normalize.css'),
+//   // page: path.resolve(__dirname, 'lib/client/js/pages'),
+//   // public: path.resolve(__dirname, 'build/public'),
+//   // server: path.resolve(__dirname, 'build/server'),
+//   // style: path.resolve(__dirname, 'client/styles')
+//   // svg: path.resolve(__dirname, 'lib/client/svg'),
+//   // video: path.resolve(__dirname, 'lib/client/video'),
+//   // web: path.resolve(__dirname, 'lib/client/web')
+// };
+
+// module.exports.PATHS = PATHS;
 
 module.exports = {
   devtool: 'source-map',
@@ -20,21 +37,36 @@ module.exports = {
   ],
   module: {
     rules: [
-    // js
-    {
-      test: /\.js$/,
-      use: ['babel-loader'],
-      include: path.join(__dirname, 'client')
-    },
-    // CSS
-    {
-      test: /\.css$/,
-      include: path.join(__dirname, 'client/styles'),
-      use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader' }
-      ]
-    }
+      // js
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        include: path.join(__dirname, 'client')
+      },
+      // CSS
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              // minimize: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              // includePaths: [PATHS.breakpoint, PATHS.normalize]
+            }
+          }
+        ]
+      }
     ]
   }
 };
