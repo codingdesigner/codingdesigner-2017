7ab505d56e27c24140069f627a47bb0c0ef0cdd8
Mason Wendell <mason@thecodingdesigner.com>
2017-12-24 16:53:11 -0500

    start portfolio section

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -5,14 +5,24 @@ import Footer from './Footer/Footer';
 
 
 class Portfolio extends React.Component {
+  // componentWillMount() {
+  //   // this runs right before the app is rendered
+  //   const PortfolioId = this.props.match.params.portfolioId;
+  //   console.log('yer mom');
+  //   console.log(PortfolioId);
+  //
+  //   import {Content} from '../data/portfolio/{PortfolioId}.json';
+  // }
+
   render() {
     return (
       <div>
         <Header/>
         <div className="page--home page-content">
           <h1 className="page-title">
-            Portfolio yo.
+            Portfolio, yo.
           </h1>
+          {this.props.match.params.portfolioId}
         </div>
         <Footer/>
       </div>
