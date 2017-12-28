b211c73fd0121b9b597d65cef4fcdcd503e60e2c
Mason Wendell <mason@thecodingdesigner.com>
2017-12-28 16:52:01 -0500

    add header randomizer to home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -8,10 +8,29 @@ import Footer from './Footer/Footer';
 
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.randomheaderRange = this.randomheaderRange.bind(this);
+    this.randomizeHeader = this.randomizeHeader.bind(this);
+
+    this.state = {
+      'randomPhoto': this.randomheaderRange()
+    };
+  }
+
+  randomheaderRange(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  randomizeHeader() {
+    const random = this.randomheaderRange();
+    this.setState({'randomPhoto': random});
+  }
+
   render() {
     return (
       <div>
-        <Header/>
+        <Header headerImage={this.state.randomPhoto} randomizeHeader={this.randomizeHeader}/>
         <div className="page--home page-content">
           <h1 className="page-title">
             Hi. <span className="my-name">I’m <span className="no-break">Mason Wendell.</span></span>
