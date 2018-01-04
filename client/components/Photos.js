c5b3122650107e14e32ac0d26334dd847bc62f86
Mason Wendell <mason@thecodingdesigner.com>
2018-01-04 14:07:45 -0500

    get a flicker photoset from api

--- client/components/Photos.js
+++ client/components/Photos.js
@@ -4,6 +4,12 @@ import LazyLoad from 'react-lazyload';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
+// Flickr
+const flickrKey = 'a7f3502c5a8c43300589c8ed4b6a01ff';
+const flickrSecret = 'b29d00d5e97d29e8';
+const flickrUser = '86001309%40N00'; // me
+const flickrPhotoset = '72157648966372560'; // Autumn/Wissahickon 2014
+
 // Behance
 const apiKey = '9P6QXs8U94R6WzJZdbQvJgMP3s91vxv8';
 const projectId = 60356123;
@@ -15,19 +21,33 @@ class Photos extends React.Component {
     this.randomheaderRange = this.randomheaderRange.bind(this);
     this.randomizeHeader = this.randomizeHeader.bind(this);
     this.displayImage = this.displayImage.bind(this);
+    this.setPhotosets = this.setPhotosets.bind(this);
 
     this.state = {
       'randomPhoto': this.randomheaderRange(),
-      'behance': []
+      'flickr': [],
+      'photosets': []
     };
   }
 
   componentDidMount () {
-    axios.get('http://behance.net/v2/projects/' + projectId + '?api_key=' + apiKey)
-      .then(res => {
-        const behance = res.data.project;
-        console.log(behance);
-        this.setState({ behance });
+    this.getFlickrPhotoset(flickrPhotoset);
+  }
+
+  randomheaderRange(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  randomizeHeader() {
+    const random = this.randomheaderRange();
+    this.setState({'randomPhoto': random});
+  }
+
+  axiosCall(url, callbackFunction) {
+    axios.get(url)
+      .then(result => {
+        console.log(result);
+        callbackFunction(result);
       })
       .catch(function (error) {
         if (error.response) {
@@ -46,16 +66,15 @@ class Photos extends React.Component {
           console.log('Error', error.message);
         }
         console.log(error.config);
-      });
+      })
   }
 
-  randomheaderRange(min = 1, max = 10) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  getFlickrPhotoset(photoset_id) {
+    const photoset = this.axiosCall('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + flickrKey + '&photoset_id=' + photoset_id + '&user_id=' + flickrUser + '&format=json&nojsoncallback=1', this.setPhotosets);
   }
 
-  randomizeHeader() {
-    const random = this.randomheaderRange();
-    this.setState({'randomPhoto': random});
+  setPhotosets(photosetItem) {
+    this.setState({ photosets : photosetItem.data.photoset });
   }
 
   displayImage(key) {
