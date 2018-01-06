dc1255d3e30807fc9cc5cfe6791d02053ac24818
Mason Wendell <mason@thecodingdesigner.com>
2018-01-05 23:01:38 -0500

    Updates

--- client/components/Photos.js
+++ client/components/Photos.js
@@ -4,6 +4,12 @@ import LazyLoad from 'react-lazyload';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
+// Flickr
+const flickrKey = 'a7f3502c5a8c43300589c8ed4b6a01ff';
+const flickrSecret = 'b29d00d5e97d29e8';
+const flickrUser = '86001309%40N00'; // me
+const flickrPhotoset = '72157648966372560'; // Autumn/Wissahickon 2014
+
 // Behance
 const apiKey = '9P6QXs8U94R6WzJZdbQvJgMP3s91vxv8';
 const projectId = 60356123;
@@ -15,19 +21,39 @@ class Photos extends React.Component {
     this.randomheaderRange = this.randomheaderRange.bind(this);
     this.randomizeHeader = this.randomizeHeader.bind(this);
     this.displayImage = this.displayImage.bind(this);
+    this.setPhotosets = this.setPhotosets.bind(this);
 
     this.state = {
       'randomPhoto': this.randomheaderRange(),
-      'behance': []
+      'photosets': []
     };
   }
 
   componentDidMount () {
-    axios.get('http://behance.net/v2/projects/' + projectId + '?api_key=' + apiKey)
-      .then(res => {
-        const behance = res.data.project;
-        console.log(behance);
-        this.setState({ behance });
+    this.getFlickrPhotoset(flickrPhotoset);
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.photosets !== this.state.photosets) {
+      console.log(this.state.photosets);
+      console.log('loop over photos in set and load images');
+    }
+  }
+
+  randomheaderRange(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  randomizeHeader() {
+    const random = this.randomheaderRange();
+    this.setState({'randomPhoto': random});
+  }
+
+  axiosCall(url, callbackFunction) {
+    axios.get(url)
+      .then(result => {
+        // console.log(result);
+        callbackFunction(result);
       })
       .catch(function (error) {
         if (error.response) {
@@ -46,16 +72,18 @@ class Photos extends React.Component {
           console.log('Error', error.message);
         }
         console.log(error.config);
-      });
+      })
   }
 
-  randomheaderRange(min = 1, max = 10) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  getFlickrPhotoset(photoset_id) {
+    const photoset = this.axiosCall('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + flickrKey + '&photoset_id=' + photoset_id + '&user_id=' + flickrUser + '&format=json&nojsoncallback=1', this.setPhotosets);
   }
 
-  randomizeHeader() {
-    const random = this.randomheaderRange();
-    this.setState({'randomPhoto': random});
+  setPhotosets(photosetItem) {
+    const photosetItemData = photosetItem.data.photoset;
+    const photosets = {...this.state.photosets};
+    photosets[photosetItemData.id] = photosetItemData;
+    this.setState({ photosets });
   }
 
   displayImage(key) {
@@ -91,7 +119,9 @@ class Photos extends React.Component {
         <div className="page--photography-page page-content">
           <h1 className="page-title">Photography</h1>
           <div className="photo-gallery">
-            {Object.keys(this.state.behance.modules).map(this.displayImage)}
+            {/*{Object.keys(this.state.behance.modules).map(this.displayImage)}*/}
+            <h3>{this.state.photosets.title}</h3>
+            {/*<img src={this.state.flickr.photo[0].iconlarge}/>*/}
           </div>
         </div>
         <Footer/>
