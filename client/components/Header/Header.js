9befdad6824adf14177e0f2d2cf7dce866b66622
Mason Wendell <mason@thecodingdesigner.com>
2018-01-14 14:12:47 -0500

    collapsible page header

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -4,8 +4,40 @@ import NavigationMain from '../Navigation/Navigation';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.collapseClass = this.collapseClass.bind(this);
+
+    this.state = {
+      'collapseHeader': false
+    };
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll (event) {
+    const scrollTop = event.target.scrollingElement.scrollTop;
+    const collapseHeader = (scrollTop > 50) ? 'collapse-header' : false;
+    this.setState({ collapseHeader });
+  }
+
+  collapseClass () {
+    if (this.state.collapseHeader !== false) {
+      return this.state.collapseHeader;
+    } else {
+      return '';
+    }
+  }
+
   render() {
-    const headerClass = 'site-header image-' + this.props.headerImage;
+    const headerClass = 'site-header image-' + this.props.headerImage + ' ' + this.collapseClass();
 
     return (
       <header className={headerClass} role="banner">
